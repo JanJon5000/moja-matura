edd9911d44e24eb720e2f68292e4dfacff04a2b6
JanJon5000 <jan@ipoczta.pl>
2021-04-09 14:29:43 +0200

    exercise 3 done

--- 2021-trial-matura-exam/zadanie4_3.py
+++ 2021-trial-matura-exam/zadanie4_3.py
@@ -1,14 +1,14 @@
 with open('C:\\VSCODE\zadania szkolne\\my-matriculation-tasks-\\2021-trial-matura-exam\\galerie.txt', 'r') as file:
     listOfVariantsWhichAlreadyAppeared = []
     CityCentreWithMaxVariants = {'city': None, 'variants':0}
-    CityCentreWithMinVariants = {'city': None, 'variants':100000}
+    CityCentreWithMinVariants = {'city': None, 'variants':100}
     for _ in range(50):
         line = file.readline()
         line = line.split(' ')
         listOfVariantsWhichAlreadyAppeared = []
         for index in range(3, len(line), 2):
             area = int(line[index-1])*int(line[index])
-            if area not in listOfVariantsWhichAlreadyAppeared:
+            if area not in listOfVariantsWhichAlreadyAppeared and area != 0:
                 listOfVariantsWhichAlreadyAppeared.append(area)
 
         if len(listOfVariantsWhichAlreadyAppeared) > CityCentreWithMaxVariants['variants']:
@@ -18,5 +18,10 @@ with open('C:\\VSCODE\zadania szkolne\\my-matriculation-tasks-\\2021-trial-matur
             CityCentreWithMinVariants['variants'] = len(listOfVariantsWhichAlreadyAppeared)
             CityCentreWithMinVariants['city'] = line[1]
 
-print(CityCentreWithMaxVariants)
-print(CityCentreWithMinVariants)
+with open('wynik4_3.txt', 'w') as file:
+    print('\t miasto o najwiekszej liczbie rodzajow lokali -',CityCentreWithMaxVariants['city'] +', liczba lokali -', CityCentreWithMaxVariants['variants'])
+    print('\t miasto o najmniejszej liczbie rodzajow lokali -',CityCentreWithMinVariants['city'] + ', liczba lokali -', CityCentreWithMinVariants['variants'])
+
+    file.write('Zadanie C\n')
+    file.write('\tmiasto o najwiekszej liczbie rodzajow lokali - ' + CityCentreWithMaxVariants['city'] + ', liczba lokali - ' + str(CityCentreWithMaxVariants['variants']) + '\n')
+    file.write('\tmiasto o najmniejszej liczbie rodzajow lokali - ' + CityCentreWithMinVariants['city'] + ', liczba lokali - ' + str(CityCentreWithMinVariants['variants']) + '\n')
