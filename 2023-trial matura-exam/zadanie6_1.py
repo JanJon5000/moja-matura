3112053e9980e215f05e5402d81ff40e9b450f57
JanJon5000 <jan@ipoczta.pl>
2021-04-16 14:09:01 +0200

    first activity

--- 2023-trial matura-exam/zadanie6_1.py
+++ 2023-trial matura-exam/zadanie6_1.py
@@ -1,4 +1,4 @@
-with open('C:\\VSCODE\\zadania szkolne\\my-matriculation-tasks-\\2023-trial matura-exam\\dane6.txt') as file:
+with open('dane6.txt') as file:
     pMinimal = {key : 0 for key in range(0, 11)}
     for _ in range(2023):
         p = 2
